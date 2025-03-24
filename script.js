2b6f5597e21e31ce620ac30fd0e7693d3d917c6a
Robby Rafky <58045054+Robby-Rafky@users.noreply.github.com>
2025-03-24 16:30:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -66,6 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const projectDetailLink = document.getElementById("project-detail-link");
 
   const projectDescriptions = {
+    "Cover Letter Agent" : "An AI agent setup to generate cover letters for an applicant, tailored to a job description and personalised to the applicant's skillset. With an easy to navigate UI and modular setup, the application can be adapted to generate anything that requires applicant information tailored to any external paramaters.",
     "3D object detection for locating telephone poles in LiDAR point clouds [In partnership with BT]": "Designed and implemented a machine learning pipeline to detect telephone poles in LiDAR point clouds as part of a Master's thesis in collaboration with BT. Utilised Python libraries such as Ultralytics and NumPy for model development, Matplotlib for visualization, and custom data augmentation techniques. 3D point clouds are encoded as 2D images for much faster processing and to single out details.",
     "Ponder LIVETRACK": "Collaborated on a university project to develop a remote water quality monitoring system, PONDER, which autonomously measures indicators such as temperature, total dissolved solids (TDS), turbidity, and pH levels. The system utilizes Arduino for sensor integration, Bluetooth for local transmission, and GSM/GPRS modules for remote communication. Data is processed and displayed on an Android application built using Kotlin, contributing to the UK's Clean Growth Grand Challenge.",
     "Audio-Visual Object Event Tracking in Time and Space": "Developed a Bachelor's project tracking objects in real time using depth inference to accurately determine object location within a 2D frame. Audio mapped to the object's location to simulate audio from that location. Other techniques such as searching a local scaled frame improves accuracy. The project provides non-sonorus objects audio for the use case of informing visually impared individuals of potential obstacles with accurate directional audio.",
@@ -89,6 +90,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const projectLinks = {
     "3D object detection for locating telephone poles in LiDAR point clouds [In partnership with BT]": "https://github.com/Robby-Rafky/3D-object-detection-for-locating-telephone-poles-in-LiDAR-point-clouds",
+    "Cover Letter Agent" : "https://github.com/Robby-Rafky/CoverLetter_Gen_Agent"
     "Ponder LIVETRACK": "https://github.com/sporadicE/PONDER",
     "Audio-Visual Object Event Tracking in Time and Space": "https://github.com/Robby-Rafky/Audio-Visual-Object-Event-Tracking-In-Time-And-Space",
     "NLP Project: Classification using NER": "https://github.com/Robby-Rafky/NLP-Project-Classification-using-Named-Entity-Recognition",
